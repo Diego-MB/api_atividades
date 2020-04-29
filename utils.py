1366a2504469027a80c09e1b995c1fbcd98ad011
Diego Maroldi Barreiros <diegomaroldi@gmail.com>
2020-04-29 15:48:42 -0300

    Adicionado models com tabelas de pessoas e atividades e criado um util de teste para manipular os dados do banco de dados.

---
+++ utils.py
@@ -0,0 +1,33 @@
+from models import Pessoas
+# Insere dados na tabela pessoa
+def insere_pessoas():
+    pessoa = Pessoas(nome='Galleani', idade=25)
+    print(pessoa)
+    pessoa.save()
+
+# Realiza consulta na tabela pessoa
+def consulta_pessoas():
+    pessoa = Pessoas.query.all()
+    print(pessoa)
+    # pessoa = Pessoas.query.filter_by(nome='Galleani').first()
+    # print(pessoa.nome)
+    # print(pessoa.idade)
+
+# Altera dados na tabela pessoa
+def altera_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Galleani').first()
+    pessoa.idade = 21
+    pessoa.save()
+
+# Exclui dados na tabela pessoa
+def exclui_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Rafael').first()
+    pessoa.delete()
+
+
+
+if __name__ == '__main__':
+    # insere_pessoas()
+    # altera_pessoa()
+    exclui_pessoa()
+    consulta_pessoas()
